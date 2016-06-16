4abf3799256f755b9fddcba8fb02f281493a458f
William Tan <williamjefftan@gmail.com>
2016-06-16 14:56:46 -0400

    Controller implements ActionListener

--- src/cs3500/music/controller/Controller.java
+++ src/cs3500/music/controller/Controller.java
@@ -1,7 +1,9 @@
 package cs3500.music.controller;
 
+import java.awt.event.ActionListener;
+
 /**
  * Interface that represents a Controller that interacts with the model & view
  */
-public interface Controller {
+public interface Controller extends ActionListener {
 }
