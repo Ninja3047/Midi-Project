738453db8441f309766a305c1ffc49a71d6c425f
William Tan <williamjefftan@gmail.com>
2016-06-18 18:58:58 -0400

    implemented the gui

--- src/cs3500/music/view/ConcreteGuiViewPanel.java
+++ src/cs3500/music/view/ConcreteGuiViewPanel.java
@@ -1,19 +1,31 @@
 package cs3500.music.view;
 
-import java.awt.*;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+import java.util.List;
 
 import javax.swing.*;
+
+import cs3500.music.controller.Controller;
+import cs3500.music.model.Note;
+import cs3500.music.model.Note.Octave;
+import cs3500.music.model.Note.Pitch;
 
 /**
  * A dummy view that simply draws a string 
  */
 public class ConcreteGuiViewPanel extends JPanel {
   private static final int CELL_SIZE = 20;
-  private static final int FONT_SIZE = 12;
+  private static final int LEFT_OFFSET = 40;
+  private final Controller<Note> controller;
 
-  public ConcreteGuiViewPanel() {
+  public ConcreteGuiViewPanel(Controller<Note> controller) {
     super();
-    this.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, FONT_SIZE));
+    this.controller = controller;
+    this.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
   }
 
   @Override
@@ -22,15 +34,85 @@ public class ConcreteGuiViewPanel extends JPanel {
     super.paintComponent(g);
     // Look for more documentation about the Graphics class,
     // and methods on it that may be useful
-    drawPitches(g, 40, 50);
+    drawMeasures(g, controller.getHighestNote(), controller.getLowestNote(), controller.getSize());
+    for (int i = 0; i < controller.getSize() + 1; i++) {
+      drawNotes(g, controller.getNotesAtBeat(i), controller.getHighestNote());
+    }
+    /*drawMeasures(g, new Note(Pitch.G, Octave.FOUR, 0, 0),
+                 new Note(Pitch.E, Octave.THREE, 0, 0), 64);
+    drawNote(g, new Note(Pitch.E, Octave.FOUR, 0, 3), Note.Utils.toInt(Pitch.G, Octave.FOUR));
+    drawNote(g, new Note(Pitch.G, Octave.FOUR, 0, 3), Note.Utils.toInt(Pitch.G, Octave.FOUR));
+    drawNote(g, new Note(Pitch.E, Octave.THREE, 0, 3), Note.Utils.toInt(Pitch.G, Octave.FOUR));
+    drawNote(g, new Note(Pitch.E, Octave.FOUR, 4, 3), Note.Utils.toInt(Pitch.G, Octave.FOUR));
+    drawNote(g, new Note(Pitch.G, Octave.FOUR, 10, 3), Note.Utils.toInt(Pitch.G, Octave.FOUR));
+    drawNote(g, new Note(Pitch.E, Octave.THREE, 12, 3), Note.Utils.toInt(Pitch.G, Octave.FOUR));*/
   }
 
-  private void drawPitches(Graphics g, int lowNote, int highNote) {
+  /**
+   * Draws the measures
+   *
+   * @param g         graphics
+   * @param highPitch highest pitch to draw
+   * @param lowPitch  lowest pitch to draw
+   * @param lastBeat  last beat to draw
+   */
+  private void drawMeasures(Graphics g, Note highPitch, Note lowPitch, int lastBeat) {
     Graphics2D g2d = (Graphics2D) g;
+
+    int pitches = highPitch.toInt() - lowPitch.toInt() + 1;
+
+    // draw horizontal note lines and pitch labels
+    Octave[] os = Octave.values();
+
+    for (int i = 0; i <= pitches; i++) {
+      g2d.drawLine(LEFT_OFFSET, CELL_SIZE * (i + 1),
+              LEFT_OFFSET + CELL_SIZE * lastBeat, CELL_SIZE * (i + 1));
+    }
+
+    for (int o = os.length - 1; o >= 0; o--) {
+      for (Pitch p : Pitch.values()) {
+        int note = Note.Utils.toInt(p, os[o]);
+        if (lowPitch.toInt() <= note && highPitch.toInt() >= note) {
+          g2d.drawString(Note.Utils.toString(p, os[o]), 0,
+                  CELL_SIZE * (highPitch.toInt() - note + 1) + (int) getFont().getSize2D());
+        }
+      }
+    }
+
+    int measures = (int) Math.ceil(lastBeat / 4.0);
+
+    // draw vertical measure lines and label every four at the top
+    for (int i = 0; i <= measures; i++) {
+      if (i % 4 == 0) {
+        g2d.drawString(Integer.toString(i * 4), LEFT_OFFSET + 4 * CELL_SIZE * i, CELL_SIZE - 4);
+      }
+      g2d.drawLine(LEFT_OFFSET + 4 * CELL_SIZE * i, CELL_SIZE,
+              LEFT_OFFSET + 4 * CELL_SIZE * i, CELL_SIZE * (pitches + 1));
+    }
   }
 
-  private void drawMeasures(Graphics g) {
-
+  /**
+   * Draws the given list of notes
+   *
+   * @param highPitch highest pitch in the composition
+   */
+  private void drawNotes(Graphics g, List<Note> notes, Note highPitch) { // draw a list of notes
+    for (Note n : notes) {
+      drawNote(g, n, highPitch);
+    }
   }
 
+  private void drawNote(Graphics g, Note n, Note highPitch) {
+    Graphics2D g2d = (Graphics2D) g;
+    int pos = n.getStart() * CELL_SIZE;
+    int pitch = highPitch.toInt() - n.toInt();
+    g2d.setColor(Color.BLACK);
+    g2d.fillRect(pos + LEFT_OFFSET, pitch * CELL_SIZE + CELL_SIZE,
+            CELL_SIZE, CELL_SIZE); // draw head
+    g2d.setColor(Color.GREEN);
+    for (int i = 1; i < n.getDuration(); i++) { // draw tail
+      g2d.fillRect(pos + i * CELL_SIZE + LEFT_OFFSET, pitch * CELL_SIZE
+              + CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    }
+  }
 }
--- src/cs3500/music/view/GuiViewFrame.java
+++ src/cs3500/music/view/GuiViewFrame.java
@@ -12,7 +12,7 @@ import cs3500.music.model.Note;
  */
 public class GuiViewFrame extends javax.swing.JFrame implements View {
 
-  private final JPanel displayPanel; // You may want to refine this to a subtype of JPanel
+  private final JScrollPane displayPanel; // You may want to refine this to a subtype of JPanel
   private final Controller<Note> controller;
 
   /**
@@ -20,9 +20,9 @@ public class GuiViewFrame extends javax.swing.JFrame implements View {
    */
   public GuiViewFrame(Controller<Note> controller) {
     this.controller = controller;
-    this.displayPanel = new ConcreteGuiViewPanel();
+    this.displayPanel = new JScrollPane(new ConcreteGuiViewPanel(controller));
     this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-    this.getContentPane().add(displayPanel);
+    this.getContentPane().add(displayPanel, BorderLayout.CENTER);
     this.pack();
   }
 
@@ -33,6 +33,6 @@ public class GuiViewFrame extends javax.swing.JFrame implements View {
 
   @Override
   public Dimension getPreferredSize(){
-    return new Dimension(1330, 400);
+    return new Dimension(1333, 400);
   }
 }
