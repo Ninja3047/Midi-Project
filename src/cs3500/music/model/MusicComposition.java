249bbee1c3a11941710b0b78911b5f14e76aff1b
William Tan <williamjefftan@gmail.com>
2016-06-19 19:05:29 -0400

    Mutate note when putting in composition

--- src/cs3500/music/model/MusicComposition.java
+++ src/cs3500/music/model/MusicComposition.java
@@ -9,6 +9,7 @@ public final class MusicComposition extends AbstractComposition<Note> {
   public void appendComposition(Composition<Note> c) {
     for (int i = 0; i < c.getLastNoteBeat() + getLastNoteBeat() + 1; i++) {
       for (Note n : c.getNotes(i)) {
+        n.setStart(i + n.getDuration() - 1);
         addNote(n, i + n.getDuration() - 1);
       }
     }
