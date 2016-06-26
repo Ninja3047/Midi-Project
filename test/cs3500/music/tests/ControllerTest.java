9ecec84043fc6f25f7bff79218b40a3f0e047026
William Tan <williamjefftan@gmail.com>
2016-06-25 23:11:35 -0400

    added more controller tests

--- test/cs3500/music/tests/ControllerTest.java
+++ test/cs3500/music/tests/ControllerTest.java
@@ -14,7 +14,7 @@ import cs3500.music.model.MusicNote.Pitch;
 import cs3500.music.model.Note;
 import cs3500.music.util.MusicReader;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 
 /**
  * Class to test the Controller Interface
@@ -46,5 +46,49 @@ public class ControllerTest {
             new MusicNote(Pitch.D, Octave.FOUR, 2, 2),
             new MusicNote(Pitch.C, Octave.FOUR, 4, 2)};
     assertArrayEquals(correct, model.getAllNotes().toArray());
+  }
+
+  @Test
+  public void testExpandNoteRange() {
+    assertEquals(64, model.getHighestNote().toInt());
+    assertEquals(55, model.getLowestNote().toInt());
+    testCon.expandNoteRange(20);
+    assertEquals(64, model.getHighestNote().toInt());
+    assertEquals(20, model.getLowestNote().toInt());
+    testCon.expandNoteRange(70);
+    assertEquals(70, model.getHighestNote().toInt());
+    assertEquals(20, model.getLowestNote().toInt());
+  }
+
+  @Test
+  public void testExpandBeatRange() {
+    assertEquals(6, model.getSize());
+    testCon.expandBeatRange(20);
+    assertEquals(20, model.getSize());
+    testCon.expandBeatRange(50);
+    assertEquals(50, model.getSize());
+  }
+
+  @Test
+  public void testContractRange() {
+    assertEquals(64, model.getHighestNote().toInt());
+    assertEquals(55, model.getLowestNote().toInt());
+    testCon.expandNoteRange(20);
+    assertEquals(64, model.getHighestNote().toInt());
+    assertEquals(20, model.getLowestNote().toInt());
+    testCon.expandNoteRange(70);
+    assertEquals(70, model.getHighestNote().toInt());
+    assertEquals(20, model.getLowestNote().toInt());
+
+    assertEquals(6, model.getSize());
+    testCon.expandBeatRange(20);
+    assertEquals(20, model.getSize());
+    testCon.expandBeatRange(50);
+    assertEquals(50, model.getSize());
+
+    testCon.contractRange();
+    assertEquals(6, model.getSize());
+    assertEquals(64, model.getHighestNote().toInt());
+    assertEquals(55, model.getLowestNote().toInt());
   }
 }
